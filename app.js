223fd3501ecc7284213975940d07d28b3b67f271
OstapoKapo <OstapoKapo@gmail.com>
2023-12-24 20:58:59 +0200

    connect to mongodb

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ const PORT = 8000;
 
 
 
+const uri = `mongodb+srv://ostapokapo:${process.env.PRIVATE_KEY}@cluster0.nbqmwbn.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(uri, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connected')
+})
+db.on('error', () => {
+    console.error(`MongoDB connection error: ${error}`)
+})
+
+
+
 app.listen(PORT, () => {
     console.log(`Server work on port ${PORT}`);
 })
